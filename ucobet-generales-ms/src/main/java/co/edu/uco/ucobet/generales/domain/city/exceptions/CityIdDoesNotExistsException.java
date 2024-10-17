3df0aa1b68eca94fc7a0c83b80ce4b45548dac83
José Miguel Posada O <jmposadao22@gmail.com>
2024-10-17 14:00:59 -0500

    Clase 10/10
    Se agregaron algunas excepciones en los paquetes de crosscutting, al mismo tiempo que se crea Repository

--- ucobet-generales-ms/src/main/java/co/edu/uco/ucobet/generales/domain/city/exceptions/CityIdDoesNotExistsException.java
+++ ucobet-generales-ms/src/main/java/co/edu/uco/ucobet/generales/domain/city/exceptions/CityIdDoesNotExistsException.java
@@ -1,7 +1,17 @@
 package co.edu.uco.ucobet.generales.domain.city.exceptions;
 
-public class CityIdDoesNotExistsException extends RuntimeException {
+import co.edu.uco.ucobet.generales.crosscutting.exceptions.RuleUcobetException;
+
+public class CityIdDoesNotExistsException extends RuleUcobetException {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	private CityIdDoesNotExistsException(final String userMessage) {
+		super(userMessage,userMessage, new Exception());
+	}
+	
+	public static final CityIdDoesNotExistsException create() {
+		var userMessage = "No existe la ciudad con el id indicado...";
+		return new CityIdDoesNotExistsException(userMessage);
+	}
 }
